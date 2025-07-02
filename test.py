2ed155fbda00a1919b236bbcfb11b19a0c2c25dd
Nkosi <njceles@gmail.com>
2025-07-02 19:35:39 +0200

    initial commit

---
+++ test.py
@@ -0,0 +1,10 @@
+def printName(name):
+    print(f"Hello, {name}!")
+
+
+firstName = "Nkosi"
+Number = 1
+
+printName(Number + 1)
+
+# ToDo - when we start reading data fromthe file , lets look at how we declare the variables
