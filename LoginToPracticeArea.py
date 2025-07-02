6c955d719f4b3c3191075cbdc8eb6ece8dbc6311
Molatelop <mulatelo@gmail.com>
2025-07-02 22:28:57 +0200

    Added a code to click on learning materials

--- LoginToPracticeArea.py
+++ LoginToPracticeArea.py
@@ -1,8 +1,10 @@
 import time
+from selenium.webdriver.support import expected_conditions as EC
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import wait
-
+from selenium.webdriver.support.wait import WebDriverWait
 
 driver=webdriver.Edge()
 driver.maximize_window()
@@ -22,22 +24,25 @@ else:
 
 time.sleep(2)
 
-driver.find_element((By.XPATH, "//*[@id='root']/div/div[1]/nav/button[7]")).click() # Click on Learning Materials
-#driver.find_element((By.XPATH, "//*[@id='root']/div/main/section/div[1]/button[5]")).click() # Click on Selenium Practice
-#driver.find_element((By.XPATH, "//*[@id='root']/div/main/section/div[2]/div/div/div[2]/div/button[1]")).click() # Click on Login to Practice Area
-#time.sleep(10)
-#element = driver.find_element(By.XPATH, "//*[@id='root']/div/main/section/div[2]/div[1]/h2[1]")
+driver.find_element(By.XPATH, "//nav//button[normalize-space(text())='Learning Materials']").click() # Click on Learning Materials
+driver.find_element(By.XPATH, "//*[@id='root']/div/main/section/div[1]/button[5]").click() # Click on Selenium Practice
+driver.find_element(By.XPATH, "/html/body/div/div/main/section/div[2]/div/div/div[1]/div/p") # Click on Login to Practice Area
+time.sleep(20)
+element = WebDriverWait(driver, 20).until(
+    EC.visibility_of_element_located((By.XPATH, "//button[normalize-space(text())='Login']"))
+)
+#element.click()
 #element.is_displayed() # Verify the Login to Practice Area text is displayed
 #Actual=element.text
 #Expected="Practice Area"
 
 #if Actual == Expected:
-    #print("Test Passed: The text is displayed correctly.")
+   # print("Test Passed: The text is displayed correctly.")
     #assert True
 #else:
-   # print(f"Test Failed: Expected '{Expected}', but got '{Actual}'.")
+    #print(f"Test Failed: Expected '{Expected}', but got '{Actual}'.")
     #assert False, f"Expected '{Expected}', but got '{Actual}'"
 
-#driver.find_element((By.XPATH, "//*[@id='root']/div/main/section/div[2]/div[1]/input")).send_keys("testuser")
+driver.find_element(By.XPATH, "//input[@placeholder='Username']").send_keys("testuser")
 #driver.find_element((By.XPATH, "//*[@id='root']/div/main/section/div[2]/div[2]/input")).send_keys("testpassword")
 #driver.find_element((By.XPATH, "//*[@id='root']/div/main/section/div[2]/div[3]/button")).click()
