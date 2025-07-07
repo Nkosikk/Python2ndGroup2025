19f39e3b7fd4ec7aee3f4aa4f85a3efcd427064b
Refilwe007 <123360859+Refilwe007@users.noreply.github.com>
2025-07-07 20:11:50 +0200

    adding having to read the username and password

--- utils/readProperties_Login.py
+++ utils/readProperties_Login.py
@@ -7,3 +7,9 @@ config.read("./Configurations/Login.ini")
 class ReadConfig_Login():
     def getDevUrl(self):
         return config.get("Common Details", "dev_url")
+
+    def getUserName(self):
+        return config.get("Login Details", "username")
+
+    def getPassword(self):
+        return config.get("Login Details", "password")
