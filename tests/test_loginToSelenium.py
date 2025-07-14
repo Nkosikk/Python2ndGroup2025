89792c3c1af8c043e62fca36b1bbdcfb730138d4
Nkosi <njceles@gmail.com>
2025-07-14 19:22:25 +0200

    added the second test

--- tests/test_loginToSelenium.py
+++ tests/test_loginToSelenium.py
@@ -1,3 +1,4 @@
+import time
 from symtable import Class
 
 import allure
@@ -13,13 +14,31 @@ class Test_LoginToSelenium:
     password = ReadConfig_Login().getpassword()
 
     @pytest.mark.sanity
+    @pytest.mark.welcomeText
     @allure.severity(allure.severity_level.CRITICAL)
     def test_verifyWelcomeText(self, setup):
         self.driver = setup
         self.driver.get(self.dev_url)
         self.hp = HomePage(self.driver)
         self.hp.verifyWelcomeText()
+        self.hp.clickLearningMaterial()
         allure.attach(self.driver.get_screenshot_as_png(), name="Home Screen",
                       attachment_type=allure.attachment_type.PNG)
+        time.sleep(5)
+
+        self.driver.quit()
+
+    @pytest.mark.sanity
+    @pytest.mark.leaning_material
+    @allure.severity(allure.severity_level.CRITICAL)
+    def test_clickLearningMaterial(self, setup):
+        self.driver = setup
+        self.driver.get(self.dev_url)
+        self.hp = HomePage(self.driver)
+        self.hp.verifyWelcomeText()
+        self.hp.clickLearningMaterial()
+        allure.attach(self.driver.get_screenshot_as_png(), name="Learning Material Page",
+                      attachment_type=allure.attachment_type.PNG)
+        time.sleep(5)
 
         self.driver.quit()
