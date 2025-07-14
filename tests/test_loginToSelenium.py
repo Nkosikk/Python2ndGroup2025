858b36b37d511635ef9674bfe71c137dfbda3bef
Nkosi <njceles@gmail.com>
2025-07-14 19:11:58 +0200

    created bat files to run reports and execute the project

--- tests/test_loginToSelenium.py
+++ tests/test_loginToSelenium.py
@@ -1,9 +1,9 @@
 from symtable import Class
 
+import allure
 import pytest
 
 from Pages.homePage import HomePage
-from basics.LoginToPracticeArea import driver
 from utils.readProperties_Login import ReadConfig_Login
 
 
@@ -13,8 +13,13 @@ class Test_LoginToSelenium:
     password = ReadConfig_Login().getpassword()
 
     @pytest.mark.sanity
+    @allure.severity(allure.severity_level.CRITICAL)
     def test_verifyWelcomeText(self, setup):
         self.driver = setup
-        self.hp=HomePage(self.driver)
+        self.driver.get(self.dev_url)
+        self.hp = HomePage(self.driver)
         self.hp.verifyWelcomeText()
-        self.driver.close()
+        allure.attach(self.driver.get_screenshot_as_png(), name="Home Screen",
+                      attachment_type=allure.attachment_type.PNG)
+
+        self.driver.quit()
